27c10e0162a75d51003b145611c5e143492fa939
RajdusF <filiprajdus5936@seznam.cz>
2025-03-31 19:55:49 +0200

    pc

--- configurator.py
+++ configurator.py
@@ -116,6 +116,7 @@ def main(args):
         
 
 if __name__ == "__main__":
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     init(autoreset=True)
     multiprocessing.freeze_support()
     parser = argparse.ArgumentParser(description="Process some integers.")
